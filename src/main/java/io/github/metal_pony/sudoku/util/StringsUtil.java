edd19aa5544543819a3d7e9f4e79516e2495c6e5
Jeff <metal-pony@users.noreply.github.com>
2026-07-30 16:47:19 -0400

    Purge non-driver files

--- src/main/java/io/github/metal_pony/sudoku/util/StringsUtil.java
+++
@@ -1,9 +0,0 @@
-package io.github.metal_pony.sudoku.util;
-
-public class StringsUtil {
-    public static String padLeft(String str, int length, char fillChar) {
-        return (length - str.length() > 0) ?
-            Character.toString(fillChar).repeat(length - str.length()) + str :
-            str;
-    }
-}
